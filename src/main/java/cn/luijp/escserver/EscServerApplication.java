fb760f3e80d6ee158ea2c71e988bf45fe576aebf
luijp <luijunpeng@outlook.com>
2024-07-02 13:06:19 +0800

    init project

---
+++ src/main/java/cn/luijp/escserver/EscServerApplication.java
@@ -0,0 +1,13 @@
+package cn.luijp.escserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/luijp/escserver/EscServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.luijp.escserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
